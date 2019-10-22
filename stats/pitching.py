07f6c3e2fb7ee7280c489c9a6770c5815475937c
davyto <55992063+davyto@users.noreply.github.com>
2019-10-22 16:30:48 +0100

    Update pitching.py

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games
+
+# Select all plays
+plays = games[games['type'] == 'play']
+
+# Select all strike outs
+strike_outs = plays[plays['event'].str.contains('K')]
+
+# Group by Year and Game
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+# Reset Index
+strike_outs = strike_outs.reset_index(name='strike_outs')
+
+# Apply an Operation to Multiple Columns
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+# Plot df
+strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend(['Strike Outs'])
+plt.show()
+
